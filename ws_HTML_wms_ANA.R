9591ea44ab658bc490ead86d6a2c6f87a602cfd2
GeomarPerales <perales.geomar@gmail.com>
2022-01-30 10:52:28 -0500

    modificado

--- ws_HTML_wms_ANA.R
+++ ws_HTML_wms_ANA.R
@@ -28,14 +28,11 @@ for (i in 1:length(tables[[1]])) {
 
 # Leyendo WMS
 library(leaflet)
-leaflet() %>% addTiles() %>% setView(-10.38, -77.5, zoom = 30) %>% addWMSTiles(links[2],
+leaflet() %>% addTiles() %>% setView(-10.38, -77.5, zoom = 1000) %>% addWMSTiles(links[2],
   layers = "0",
   options = WMSTileOptions(format = "image/png", transparent = TRUE),
   attribution = "") 
 
 
 # WMS
-
-
-
-plot(sic)
+# NO se puede visualizar, dado que los links no están en el formato adecuado %>% %>% 
