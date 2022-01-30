70c7c9d674da5e604a1ba9091b71ae14efff1901
GeomarPerales <perales.geomar@gmail.com>
2022-01-30 13:10:55 -0500

    modificado

--- ws_wfs_geoidep.R
+++ ws_wfs_geoidep.R
@@ -1,4 +1,7 @@
 library(rvest)
 url_gidep_wfs <- "https://www.geoidep.gob.pe/catalogo-nacional-de-servicios-web/servicios-de-publicacion-de-objetos-wfs"
 data_inn_ws <- read_html(url_gidep_wfs)
-tables <- html_nodes(data_inn_ws, "table")
+headers <- data_inn_ws %>% html_nodes("option") %>% html_text()
+
+#la función rvest no lee el enlace del geoidep - 
+#catalogo nacional de servicios web de Perú
