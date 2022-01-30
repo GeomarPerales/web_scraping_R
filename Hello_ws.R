82c7546f7282e67dd2b639c5f6be12284d5f0908
GeomarPerales <perales.geomar@gmail.com>
2022-01-30 00:48:03 -0500

    script modificado

--- Hello_ws.R
+++ Hello_ws.R
@@ -1,2 +1,9 @@
-library(datasets)
-data("LakeHuron")
+library(rvest)
+
+url_senamhi <- "https://repositorio.senamhi.gob.pe/handle/20.500.12542/1215"
+
+data_ini <- read_html(url_senamhi)
+View(data_ini)
+
+lines <- readLines(con = url_senamhi)
+View(lines)
