ad23c458ec2e9a2b92233e6d8009aa8bfcc33505
GeomarPerales <perales.geomar@gmail.com>
2022-01-30 12:19:40 -0500

    modificado

--- ws_wms_sinia_minam.R
+++ ws_wms_sinia_minam.R
@@ -4,3 +4,6 @@ sinia_minam_html <- read_html(url_sinia_minam_wms)
 
 headers <- sinia_minam_html %>% html_nodes("h4") %>% html_text()
 txt_headers <- sinia_minam_html %>% html_nodes("a") %>% html_text()
+txt_headers <- data.frame(txt = txt_headers)
+
+links <- subset(txt_headers, grepl("http", txt))
